ee9095f4e3a0e9253e3d894c045e7d3ad20939c0
Vivekbhardwaj <vivekbhardwaj0067@gmail.com>
2022-05-30 02:04:30 +0530

    template for portfolio.py and placeholders

---
+++ portfolio.py
@@ -0,0 +1,19 @@
+
+
+# returns a dictionary d
+# schema of dict
+'''
+{
+    "pie_markowitz" : "path to pie chart of markowitz (uuid).png",
+    pie chart for all three
+    "barchart_comparision......." : "paths",
+
+    {
+        "Ticker1" : "No of stocks to buy",
+        "Ticker2" : "No of stocks",
+        .
+    }
+}
+'''
+def improve(ticker,amt) :
+    pass
